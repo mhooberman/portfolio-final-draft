8051e8634d92dd100e743acc8306414ac220a817
mhooberman <90577214+mhooberman@users.noreply.github.com>
2022-04-12 15:38:51 -0400

    Add files via upload

---
+++ js/aboutme.js
@@ -0,0 +1,11 @@
+document.querySelector("#ttt_button").addEventListener("click", function() {
+	console.log("Clicked to view @twinstalktaste");
+    window.alert("You clicked a button to view @twinstalktaste. If you wish to view it, press 'OK' and you will be taken to a separate page on Instagram.")
+    window.open("https://www.instagram.com/twinstalktaste/")
+});
+
+document.querySelector("#workout").addEventListener("click", function() {
+	console.log("Clicked to view Marlee's favorite workout video");
+    window.alert("You clicked a button to view Marlee's favorite workout video. If you wish to view it, press 'OK' and you will be taken to a separate page on YouTube.")
+    window.open("https://www.youtube.com/watch?v=MTF8-wuTH4Y")
+});
---
+++ js/contact.js
@@ -0,0 +1,5 @@
+document.querySelector("#linked_in").addEventListener("click", function() {
+	console.log("Clicked to view @twinstalktaste");
+    window.alert("You clicked a button to view Marlee's personal LinkedIn page. If you wish to view it, press 'OK' and you will be taken to a separate page on LinkedIn.")
+    window.open("https://www.linkedin.com/in/marlee-hooberman/")
+});
