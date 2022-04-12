c2c82450daf4260da9a4c7200e3c7aade5d0547c
mhooberman <90577214+mhooberman@users.noreply.github.com>
2022-04-12 13:32:11 -0400

    Add files via upload

---
+++ js/myexp.js
@@ -0,0 +1,5 @@
+document.querySelector("#resume").addEventListener("click", function() {
+	console.log("Clicked resume");
+    window.alert("You clicked a PDF file. If you wish to view it, press 'OK' and you will be taken to a separate page to view it.")
+});
+
