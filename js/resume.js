fdd0838fc8aea20b4ed3e29335d600acab66601c
mhooberman <90577214+mhooberman@users.noreply.github.com>
2022-04-12 11:39:29 -0400

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,4 @@
 document.querySelector("#resume").addEventListener("click", function() {
 	console.log("Clicked resume");
-    window.alert("You clicked a PDF file")
+    window.alert("You clicked a PDF file. If you wish to view it, press 'OK' and you will be taken to a separate page to view it.")
 });
